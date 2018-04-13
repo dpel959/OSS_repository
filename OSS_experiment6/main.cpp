e0032a7e20da45ebf8ade71cdce976cbfad6bf85
tkdrb15 <ekffltkdrb@naver.com>
2018-04-13 10:35:06 +0900

    i don t know

--- OSS_experiment6/main.cpp
+++ OSS_experiment6/main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 int main()
 {
 	cout << "구구단 출력" << endl;
-	for (int i = 1; i < 9; i++) {
+	for (int i = 1; i <= 9; i++) {
 		cout << i << " * " << i << " = " << i * i << endl;
 	}
-	system("pause");
 	return 0;
 }
