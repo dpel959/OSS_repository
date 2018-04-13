6203eaf30fffb47e5a1d149888efad86981d2805
dpel959 <dpel959@naver.com>
2018-04-13 10:14:50 +0900

    multiply99

---
+++ OSS_experiment6/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "구구단 출력" << endl;
+	for (int i = 1; i < 9; i++) {
+		cout << i << " * " << i << " = " << i * i << endl;
+	}
+	system("pause");
+	return 0;
+}
